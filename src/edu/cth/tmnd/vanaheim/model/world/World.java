e782a1ae92b438ea5f682a3ae153741cbb259fbf
Richard <Richard@Dashie>
2013-04-17 11:53:51 +0200

    New attempt to upload the map.

--- src/edu/cth/tmnd/vanaheim/model/world/World.java
+++ src/edu/cth/tmnd/vanaheim/model/world/World.java
@@ -22,7 +22,7 @@ public class World {
 		this.map = map;
 		for (int i = 0; i < map.getHeight(); i++) {
 			for (int j = 0; j < map.getWidth(); j++) {
-				//System.out.println(map.getTileId(j, i, 1));
+				//System.out.println(map.getTileId(j, i, 2));
 				if (map.getTileId(j, i, 0) == 178) {
 					tiles[j][i] = new GrassTile();
 				}
@@ -43,15 +43,15 @@ public class World {
 	public void changeTile(int x, int y) {
 		int xPos = (int)Math.floor(x / 32);
 		int yPos = (int)Math.floor(y / 32);
-		if (map.getTileId(xPos, yPos, 2) != 285) {
-			map.setTileId(xPos, yPos, 2, 285);
+		if (map.getTileId(xPos, yPos, 2) != 257) {
+			map.setTileId(xPos, yPos, 2, 257);
 		}
 	}
 
 	public void lootAll(int x, int y) {
 		int xPos = (int)Math.floor(x / 32);
 		int yPos = (int)Math.floor(y / 32);
-		if (map.getTileId(xPos, yPos, 2) == 285) {
+		if (map.getTileId(xPos, yPos, 2) == 257) {
 			System.out.println("Här finns loot");
 		}
 	}
