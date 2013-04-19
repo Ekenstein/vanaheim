552ce6b46ce47a908a8a6ec1931f7c7510f4b2c9
gabriel.ekblad <gabriel.ekblad@gmail.com>
2013-04-19 09:12:43 +0200

    Added direction to creatures. This should be used in order to know which sprite to render on the screen for certain directions of creatures. Also added to the controller possibility of fetching this by getPlayerDirection()

--- src/edu/cth/tmnd/vanaheim/controller/Controller.java
+++ src/edu/cth/tmnd/vanaheim/controller/Controller.java
@@ -2,13 +2,11 @@ package edu.cth.tmnd.vanaheim.controller;
 
 import java.awt.Point;
 
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 import edu.cth.tmnd.vanaheim.model.Inventory;
 import edu.cth.tmnd.vanaheim.model.Trie.Trie;
+import edu.cth.tmnd.vanaheim.model.creatures.impl.Creature.Direction;
 import edu.cth.tmnd.vanaheim.model.creatures.player.Player;
 import edu.cth.tmnd.vanaheim.model.items.impl.Item;
 import edu.cth.tmnd.vanaheim.model.world.World;
@@ -16,70 +14,74 @@ import edu.cth.tmnd.vanaheim.model.world.World;
 
 
 public class Controller {
-	private Player player;
+	private final Player player;
 	private Trie lexicon;
-	private World world;
-	
+	private final World world;
+
 	public Controller() {
 		world = new World();
 		player = new Player(32f, 32f, 300, new Inventory(20), 100);
 	}
-	
+
 	public Point getPlayerLoc() {
 		return player.getLoc();
 	}
-	
-	public void setPlayerLoc(Point p) {
+
+	public void setPlayerLoc(final Point p) {
 		player.setPlayerLoc(p);
 	}
-	
-	public boolean isBlocked(int x, int y) {
+
+	public boolean isBlocked(final int x, final int y) {
 		return world.isBlocked(x, y);
 	}
-	
+
 	public TiledMap getMap() {
 		return world.getMap();
 	}
-	
-	public void initMap(TiledMap map) {
+
+	public void initMap(final TiledMap map) {
 		world.initMap(map);
 	}
-	
-	public void checkTile(int x, int y) {
+
+	public void checkTile(final int x, final int y) {
 		world.checkTile(x, y);
 	}
-	
-	public void changeTile(int x, int y) {
+
+	public void changeTile(final int x, final int y) {
 		world.changeTile(x, y);
 	}
-	
-	public void lootAll(int x, int y) {
+
+	public void lootAll(final int x, final int y) {
 		world.lootAll(x, y);
 	}
-	
-	public void dropItem(Item item) {
-		Item i = this.player.getItem(item);
-		
+
+	public void dropItem(final Item item) {
+		final Item i = this.player.getItem(item);
+
 		if(i != null) {
-			float x = this.player.getX();
-			float y = this.player.getY();
-			
+			final float x = this.player.getX();
+			final float y = this.player.getY();
+
 			if(this.world.addItemToTile(x, y, i)) {
 				this.player.removeItem(i);
 			}
 		}
 	}
-	
-	public boolean hasTileItems(float x, float y) {
+
+	public boolean hasTileItems(final float x, final float y) {
 		return this.world.hasTileItems(x, y);
 	}
-	
-	public Inventory getTileItems(float x, float y) {
+
+	public Inventory getTileItems(final float x, final float y) {
 		if(this.hasTileItems(x, y)) {
 			return this.world.getTileInventory(x, y);
 		}
-		
+
 		return null;
+	}
+
+	public Direction getPlayerDirection() {
+		return this.player.getDirection();
 	}
 }
 
--- src/edu/cth/tmnd/vanaheim/model/creatures/impl/Creature.java
+++ src/edu/cth/tmnd/vanaheim/model/creatures/impl/Creature.java
@@ -7,78 +7,108 @@ import edu.cth.tmnd.vanaheim.model.items.impl.Item;
 
 public abstract class Creature extends GameObject {
 
+	public static enum Direction {
+		LEFT, RIGHT, UP, DOWN
+	};
 	protected int velocity;
 	protected Inventory inventory;
 	protected int currentHP, maxHP;
 	protected EquipableItem equipment;
-	
-	public Creature(float x, float y, int velocity, Inventory inventory, int maxHP) {
+	protected Direction currentDirection;
+
+
+	public Creature(final float x, final float y, final int velocity, final Inventory inventory, final int maxHP) {
 		super(x, y);
 		this.velocity = velocity;
 		this.inventory = inventory;
 		this.maxHP = this.currentHP = maxHP;
 	}
-	
+
 	public int getVelocity() {
 		return this.velocity;
 	}
-	
-	public void equip(EquipableItem item) {
+
+	public void equip(final EquipableItem item) {
 		if(this.equipment != null) {
 			return;
 		}
-		
+
 		this.equipment = (EquipableItem) this.inventory.retrieveItem(item);
 	}
-	
+
 	public void unequip() {
 		this.equipment = this.inventory.addItem(this.equipment) ? null : this.equipment;
 	}
-	
-	public void setMaxHP(int hp) {
+
+	public void setMaxHP(final int hp) {
 		this.maxHP = hp;
 	}
-	
-	public int heal(int hp) {
-		int healed = this.currentHP + hp;
-		
+
+	public int heal(final int hp) {
+		final int healed = this.currentHP + hp;
+
 		this.currentHP = healed > this.maxHP ? this.maxHP : healed;
 		return healed - this.currentHP;
 	}
-	
-	public void damage(int damage) {
-		this.currentHP -= damage > this.currentHP ? this.currentHP : damage; 
+
+	public void damage(final int damage) {
+		this.currentHP -= damage > this.currentHP ? this.currentHP : damage;
 	}
-	
+
 	public boolean isDead() {
 		return this.currentHP == 0;
 	}
-	
-	public void setVelocity(int velocity) {
+
+	public void setVelocity(final int velocity) {
 		this.velocity = velocity;
 	}
-	
-	public boolean addItem(Item item) {
+
+	public boolean addItem(final Item item) {
 		return this.inventory.addItem(item);
 	}
-	
-	public Item retrieveItem(Item item) {
+
+	public Item retrieveItem(final Item item) {
 		return this.inventory.retrieveItem(item);
 	}
 
-	public boolean destroyItem(Item item) {
+	public boolean destroyItem(final Item item) {
 		return this.inventory.destroyItem(item);
 	}
-	
-	public Item removeItem(Item item) {
+
+	public Item removeItem(final Item item) {
 		return this.inventory.removeItem(item);
 	}
-	
-	public Item getItem(Item item) {
+
+	public Item getItem(final Item item) {
 		return this.inventory.getItem(item);
 	}
-	
+
 	public int getHP() {
 		return this.currentHP;
+	}
+
+	@Override
+	public void setX(final float x) {
+		if(super.x > x) {
+			this.currentDirection = Direction.LEFT;
+		} else if(super.x < x) {
+			this.currentDirection = Direction.RIGHT;
+		}
+		super.x = x;
+	}
+
+	@Override
+	public void setY(final float y) {
+		if(super.y > y) {
+			this.currentDirection = Direction.UP;
+		} else if(super.y < y) {
+			this.currentDirection = Direction.DOWN;
+		}
+
+		super.y = y;
+	}
+
+	public Direction getDirection() {
+		return this.currentDirection;
 	}
 }
