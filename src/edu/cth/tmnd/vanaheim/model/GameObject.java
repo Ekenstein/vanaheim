f7a75fcbdbd29c4ae9ac4b7185ef822ee4f3e634
gabriel.ekblad <gabriel.ekblad@gmail.com>
2013-04-20 12:24:09 +0200

    Removed unnecessary import

--- src/edu/cth/tmnd/vanaheim/model/GameObject.java
+++ src/edu/cth/tmnd/vanaheim/model/GameObject.java
@@ -1,29 +1,27 @@
 package edu.cth.tmnd.vanaheim.model;
 
-import org.newdawn.slick.Animation;
-
 public abstract class GameObject {
-	
+
 	protected float x, y;
-	
-	public GameObject(float x, float y) {
+
+	public GameObject(final float x, final float y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public float getX() {
 		return this.x;
 	}
-	
+
 	public float getY() {
 		return this.y;
 	}
-	
-	public void setX(float x) {
+
+	public void setX(final float x) {
 		this.x = x;
 	}
-	
-	public void setY(float y) {
+
+	public void setY(final float y) {
 		this.y = y;
 	}
 }
