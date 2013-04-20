4e96edd6aedf5e7b7b1635d2a983bba21737c838
Daniel Jonsson <damajo90@gmail.com>
2013-04-20 11:39:25 +0200

    Added tests for quest and added some new quests and items. Also fixed bugs that was found during the testing

--- src/edu/cth/tmnd/vanaheim/model/creatures/npc/impl/NPC.java
+++ src/edu/cth/tmnd/vanaheim/model/creatures/npc/impl/NPC.java
@@ -1,14 +1,15 @@
 package edu.cth.tmnd.vanaheim.model.creatures.npc.impl;
 
+import edu.cth.tmnd.vanaheim.model.Inventory;
 import edu.cth.tmnd.vanaheim.model.creatures.impl.Human;
 
-public class NPC {
+public class NPC extends Human {
 
 	State s;
 	State next;
 	
-	public NPC(){
-		
+	public NPC(float x, float y, int velocity, Inventory inventory, int maxHp){
+		super(x, y, velocity, inventory, maxHp);
 	}
 	
 	public void talk(Human human){
--- src/edu/cth/tmnd/vanaheim/model/quests/SilverQuest.java
+++ src/edu/cth/tmnd/vanaheim/model/quests/SilverQuest.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 import edu.cth.tmnd.vanaheim.model.creatures.impl.Creature;
 import edu.cth.tmnd.vanaheim.model.quests.impl.Quest;
 
-final public class GunnarQuest extends Quest {
+final public class SilverQuest extends Quest {
 
 	
-	public GunnarQuest(Creature owner) {
+	public SilverQuest(Creature owner) {
 
-		super("Gold mining", "Mine three good lugs of gold and then return to me for your reward", owner, new HashMap<String,Integer>());
+		super("Silver mining", "Mine three good lugs of silver and then return to me for your reward", owner, new HashMap<String,Integer>());
 		this.addNeededItemCount("Gold", 3);
 	}
 
