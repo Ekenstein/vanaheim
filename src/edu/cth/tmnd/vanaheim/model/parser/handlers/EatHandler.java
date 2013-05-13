b3e05651c82cdd1dbe97113f35c08e2b4b1bc3bd
Daniel Jonsson <damajo90@gmail.com>
2013-05-13 23:33:54 +0200

    Implemented eathandler

--- src/edu/cth/tmnd/vanaheim/model/parser/handlers/EatHandler.java
+++ src/edu/cth/tmnd/vanaheim/model/parser/handlers/EatHandler.java
@@ -1,18 +1,35 @@
 package edu.cth.tmnd.vanaheim.model.parser.handlers;
 
+import edu.cth.tmnd.vanaheim.model.items.impl.DrinkableItem;
+import edu.cth.tmnd.vanaheim.model.items.impl.EatableItem;
 import edu.cth.tmnd.vanaheim.model.parser.handlers.impl.Handler;
 
 final public class EatHandler extends Handler {
 
+	private final static int ITEM = 0;
+	
 	@Override
 	protected boolean checkArgs(final Object[] args) {
-		// TODO Auto-generated method stub
-		return false;
+		if(args == null) {
+			return false;
+		}
+		
+		if(args.length != 1) {
+			return false;
+		}
+		
+		if(!(args[ITEM] instanceof EatableItem)) {
+			return false;
+		}
+		
+		return true;
 	}
 
 	@Override
 	protected void handleArgs(final Object[] args) {
-		// TODO Auto-generated method stub
+		EatableItem item = (EatableItem) args[ITEM];
+		
+		item.eat(super.p);
 
 	}
 }
